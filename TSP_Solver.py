045b8ce5b0794474906c4eb060bdfa1a586fabf3
Marcob1996 <mib2@rice.edu>
2018-04-13 23:28:37 -0500

    I like to push

--- TSP_Solver.py
+++ TSP_Solver.py
@@ -8,13 +8,16 @@ filename = r"C:\Users\marco_000\Traveling-Salesman-Problem_new\Data\synthetic8.t
 
 weights = DataIO.read_graph(filename)
 print(weights)
+num_nodes = len(weights)
 # Create Model
 m = Model("TSP")
-
 # Create variables
-variables = {}
+variables = []
 for i in range(num_nodes):
-    for j in range(i+1):
-        variables[i, j] = m.addVar(obj= weights[i, j], vtype=GRB.BINARY, name='e'+str(i)+'_'+str(j))
-        variables[j, i] = variables[i, j]
-#m.update()
+    for j in weights[i].keys():
+        if j in weights[i]:
+            if not (j, i) in variables:
+                m.addVar(obj=weights[i][j], vtype=GRB.BINARY, name='e'+str(i)+'_'+str(j))
+                variables.append((i, j))
+m.update()
+print(variables)
