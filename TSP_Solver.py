fc8e3631bb98e370f65d0d0fb212d58a4f2e4fcf
Marcob1996 <mib2@rice.edu>
2018-04-14 16:43:32 -0500

    I like to push  (added output file)

--- TSP_Solver.py
+++ TSP_Solver.py
@@ -8,7 +8,7 @@ from UndirectedGraph import UndirectedGraph
 t0 = time()
 
 # filename = r"C:\Users\marco_000\Traveling-Salesman-Problem_new\Data\synthetic8.txt"  # for Marco
-filename = "./Data/gr21.txt"  # for Rangan
+filename = "./Data/ulysses22.txt"  # for Rangan
 
 weights = DataIO.read_graph(filename)
 
@@ -74,6 +74,6 @@ while True:
         break
 
 t1 = time()
-m.write("gr21_out5.lp")
+m.write("ulysses22_out10.lp")
 print "Total time taken: " + str(t1 - t0) + " seconds"
 
