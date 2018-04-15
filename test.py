816753b6addae010114e45471e4e63e3db7dc56c
Marcob1996 <mib2@rice.edu>
2018-04-15 18:55:39 -0500

    I like to push  (added output file)

--- test.py
+++ test.py
@@ -6,18 +6,45 @@ from UndirectedGraph import UndirectedGraph
 
 t_0 = time()
 
-filename = "att48.txt"
+graph_filename = "att48.txt"
 
-dict_graph = DataIO.read_graph("./../Data/" + filename)
+relative_path = "./Data/" + graph_filename
+
+dict_graph = DataIO.read_graph(relative_path)
 
 G = UndirectedGraph.dictionary_to_undirected_graph_form(dict_graph)
 
-minimum_cut, minimum_cut_weight = StoerWagner.apply(G, G.get_node_names().pop())
+
+def minimum_spanning_tree(dict1):
+    mst = []
+    weight = 0
+    mst.append(0)
+    while len(mst) != len(dict1):
+        min2 = float('inf')
+        node_add = 0
+        new_w = 0
+        for j in mst:
+            inner_dict = dict1[j]
+            for k in inner_dict:
+                if inner_dict[k] < min2 and k not in mst:
+                    min2 = inner_dict[k]
+                    new_w = min2
+                    node_add = k
+        mst.append(node_add)
+        weight += new_w
+    return mst, weight
+
+m, w = minimum_spanning_tree(dict_graph)
+# minimum_cut, minimum_cut_weight = StoerWagner.apply(G, G.get_node_names().pop())
 
 t_1 = time()
 
-print "\nMinimum Cut: " + str(minimum_cut) + "\n"
-print "Minimum Cut Weight: " + str(minimum_cut_weight) + "\n"
+print(m)
+print(w)
+print(str(t_1 - t_0))
 
-print "Time Taken: " + str(t_1 - t_0) + "s\n"
+# print "\nMinimum Cut: " + str(minimum_cut) + "\n"
+# print "Minimum Cut Weight: " + str(minimum_cut_weight) + "\n"
+
+# print "Time Taken: " + str(t_1 - t_0) + "s\n"
 
