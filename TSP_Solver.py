3c6f6083a7e5aa5be9f0d6d08cba97c245af1da0
Rangan <rm48@rice.edu>
2018-04-15 19:09:40 -0500

    added timing

--- TSP_Solver.py
+++ TSP_Solver.py
@@ -5,9 +5,29 @@ from StoerWagner import StoerWagner
 from UndirectedGraph import UndirectedGraph
 
 
+def minimum_spanning_tree(dict1):
+    mst = []
+    weight = 0
+    mst.append(0)
+    while len(mst) != len(dict1):
+        min2 = float('inf')
+        node_add = 0
+        new_w = 0
+        for j in mst:
+            inner_dict = dict1[j]
+            for k in inner_dict:
+                if inner_dict[k] < min2 and k not in mst:
+                    min2 = inner_dict[k]
+                    new_w = min2
+                    node_add = k
+        mst.append(node_add)
+        weight += new_w
+    return mst, weight
+
+
 t0 = time()
 
-graph_filename = "berlin52.txt"
+graph_filename = "ulysses22.txt"
 
 relative_path = "./Data/" + graph_filename
 
@@ -18,6 +38,8 @@ num_nodes = len(weights)
 # Create Model
 model = Model("TSP")
 
+obj = LinExpr()
+
 # Create variables
 variables = {}
 for i in range(num_nodes):
@@ -25,6 +47,7 @@ for i in range(num_nodes):
         if j in weights[i]:
             if not (j, i) in variables:
                 variable = model.addVar(obj=weights[i][j], vtype=GRB.BINARY, name='e' + str(i) + '_' + str(j))
+                obj.add(variable, weights[i][j])
                 variables[tuple((i, j))] = variable
 
 # Add Degree-2 Constraints
@@ -34,6 +57,10 @@ for i in weights.keys():
         if i in pair:
             lhs.add(var)
     model.addConstr(lhs == 2)
+
+mst, mst_weight = minimum_spanning_tree(weights)
+
+model.addConstr(obj <= 2 * mst_weight)  # upper bound
 
 iter_index = 1
 
